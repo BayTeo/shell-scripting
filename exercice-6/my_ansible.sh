2568f036059453e0364ec8faf665c745f658099f
BayTeo <teo.bay@edu.devinci.fr>
2021-12-14 19:04:04 +0100

    Commit de l'exercice 6

---
+++ exercice-6/my_ansible.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p documents/{gamez,images,documents,work} documents/work/{code,planning,}
+touch meeting_notes.txt documents/work/planning/{april,february,january,march}.xlsx documents/gamez/csgo.exe
