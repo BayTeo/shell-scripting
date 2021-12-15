00166c8af441327ef5937afd9cd638b250976f69
BayTeo <teo.bay@edu.devinci.fr>
2021-12-15 07:49:19 +0100

    Rectification du nom de l'exercice

---
+++ exercice-5/salutation_advanced.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+if (($# < 2)) && (($# > 2)); then
+	elif [ $1 = $2 ]; then
+		echo "Firstname and lastname cannot be the same!"
+		exit 2
+	else 
+	        echo "Usage : ./salutation.sh firstname lastname"
+        exit 1
+fi
+    echo "Hello $1 $2, have a nice day !"
