3411582582e84c6f575acb86e1a6dafc9df24e5c
BayTeo <76566461+BayTeo@users.noreply.github.com>
2021-12-15 07:42:33 +0100

    Update salutations advanced.sh

--- exercice-5/salutations advanced.sh
+++ exercice-5/salutations advanced.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 if (($# < 2)) && (($# > 2)); then
-        echo "Usage : ./salutation.sh firstname lastname"
+	elif [ $1 = $2 ]; then
+		echo "Firstname and lastname cannot be the same!"
+		exit 2
+	else 
+	        echo "Usage : ./salutation.sh firstname lastname"
         exit 1
-elif [ $1 = $2 ]; then
-	echo "Firstname and lastname cannot be the same!"
-	exit 2
-
 fi
     echo "Hello $1 $2, have a nice day !"
