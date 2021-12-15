4d5809f18bc7a8bcf01944f0ce7b1628301d6a57
Arnaud Jacques <arnaud.jacquesedu.devinci.fr>
2021-12-15 08:38:21 +0100

    Rectification de l'exercice 5

--- exercice-5/salutation_advanced.sh
+++ exercice-5/salutation_advanced.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-if (($# < 2)) && (($# > 2)); then
-	elif ( $1 = $2 ); then
-		echo "Firstname and lastname cannot be the same!"
-		exit 2
-	else 
-	        echo "Usage : ./salutation.sh firstname lastname"
-        exit 1
-fi
-    echo "Hello $1 $2, have a nice day !"
+if (($# < 2)) || (($# > 2)) ;then
+		echo "Usage : ./salutation.sh firstname lastname"
+		exit 1
+	elif [ $1 = $2 ]; then 
+	        echo "Firstname and lastname cannot be the same!"
+	  	exit 2
+	fi
+    		echo "Hello $1 $2, have a nice day !"
+    		exit 0
+    		
